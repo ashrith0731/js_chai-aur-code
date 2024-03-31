f5f766068342f0effa8028e4ad2518f5889fac53
vj0731 <90322573+ashrith0731@users.noreply.github.com>
2024-03-31 08:26:16 +0000

    starting with 01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "vits3305@gmail.com"
+var accountPassword = "12345"
+accountCity = "Leicester"
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "2121212"
+accountCity = "London"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
